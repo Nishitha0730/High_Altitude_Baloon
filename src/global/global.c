100173f07a221631181f5d4675c81479e1f3a05c
Nishitha0730 <pitigalapknw.22@uom.lk>
2026-08-29 12:42:55 +0530

    Add array named full_data[17] to send imu data with predefined header, prevent data misaligned

--- src/global/global.c
+++ src/global/global.c
@@ -5,3 +5,4 @@
 uint8_t empty_data_reg=0;
 
 uint8_t imu_buf[14];
+uint8_t full_data[17]={[0]=0xFF,0xFF,0xFF};
--- src/global/global.h
+++ src/global/global.h
@@ -4,6 +4,6 @@
 extern uint8_t empty_data_reg;
 
 extern uint8_t imu_buf[14];
-
+extern uint8_t full_data[17];
 
 #endif
