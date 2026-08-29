48e949760c99ab79364e749485f24db40761a451
Nishitha0730 <pitigalapknw.22@uom.lk>
2026-08-29 16:48:23 +0530

    use DWT to find clk cycle usage for imu task

--- src/global/global.h
+++ src/global/global.h
@@ -4,6 +4,12 @@
 extern uint8_t empty_data_reg;
 
 extern uint8_t imu_buf[14];
-extern uint8_t full_data[17];
+// extern uint8_t full_data[17];
+
+extern uint8_t full_data[21];
+
+extern uint32_t start;
+extern uint32_t end;
+extern uint32_t cycles;
 
 #endif
